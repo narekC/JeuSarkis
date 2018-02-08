1812d669e97bb6c1f0ee342783406a0b92326963
Narek <35571991+narekC@users.noreply.github.com>
2018-02-08 10:56:16 +0100

    jeuSako

---
+++ narek/script.js
@@ -0,0 +1,13 @@
+nom (
+{
+var prenom = Narek;
+var nom = Hakobyan;
+var age = 26;
+
+course = Number(document.getElementById("course").value)
+nrOfLessons = Number(document.getElementById("nrOfLessons").value)
+
+nom = Text(document.getElementById("nom").value("hakobyan"));
+prenom = Text(document.getElementById("prenom").value('Narek') );
+age = Number(document.getElementById("age").value ("26")) ;
+})
